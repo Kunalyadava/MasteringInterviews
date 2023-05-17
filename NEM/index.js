d9fdc50fc5fc51c096aa93256276296b6d8e1493
Ku Nal <104050919+Kunalyadava@users.noreply.github.com>
2023-05-18 02:07:48 +0530

    practice added

---
+++ NEM/index.js
@@ -0,0 +1,18 @@
+
+   //Node JS 
+//    What is Node.js?
+//    NodeJS is an open-source and cross-platform JavaScript runtime environment built on top of Chrome’s 
+//    V8 JavaScript Engine enabling high performance and scalability. NodeJS uses the process model instead 
+//    of the traditional web server model, which makes it super useful for web development.
+// Advantages of Node.js
+// The popularity of Node.js can be attributed to a couple of stand-out advantages, some of them alluded to in the previous section.
+
+// Node.js is open-source meaning it has been made freely available to developers all over the world.
+//  Anyone is able to proposed modifications or improvements.
+// Using Node.js we are able to build entire server-side applications using only JavaScript.
+// The runtime environment itself is very lightweight. It includes only the core modules
+//  needed to function. Any other modules required by the application may be easily installed.
+// The asynchronous nature of Node.js leads to increased performance over other frameworks.
+// Node.js may be run on Windows, MAC, or Linux systems.
+
+//https://medium.com/@kunalkr/what-is-nodejs-process-model-55df68bccc9e
